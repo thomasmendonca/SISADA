9cf9d10a647536b2a44824c5da48352d020d374c
thomasmendonca <rdcharacter10@gmail.com>
2026-08-31 21:20:05 -0300

    chore: update gitignore rules

---
+++ backend/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/health')
+async def home():
+    return {'mensagem':'Funcionando'}
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=3002,
+    )
